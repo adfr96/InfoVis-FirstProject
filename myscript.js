769d344210de923f6802780a0c7fbc614eb527b5
adfr96 <alexdellorco@gmail.com>
2020-05-30 13:13:26 +0200

    add first chart

--- myscript.js
+++ myscript.js
@@ -1,18 +1,65 @@
-var data
-d3.json("data.json").then(function(d) {
-    var p = d3.select("body")
-        .selectAll("p")
-        .data(d)
-        .text(function(d) { return d.x1; });
-    p.enter().append('p').text(function(d){return d.x1;})
-  });
-  
+var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 1000 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-console.log(data)
-/*
-var p = d3.select("body")
-        .selectAll("p")
-        .data(data)
-        .text(function(d) { return d.x1; });
-p.enter().append('p').text(function(d){return d.x1;})
-*/
+var chart_width = width/2
+var chart_height = height/2
+
+var x = d3.scaleBand().rangeRound([0, chart_width]).padding(0.5);
+
+var y = d3.scaleLinear().range([chart_height, 0]);
+
+var xAxis = d3.axisBottom(x);
+
+var yAxis = d3.axisLeft(y).ticks(10);
+    
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+function comp_x1(v1,v2){
+  if(v1.x1>v2.x1){
+    return -1
+  }
+  else if(v2.x1>v1.x1){
+    return 1
+  }
+  return 0
+}
+d3.json("data.json").then(function(data) {
+    data.sort(comp_x1)
+    console.log(data)
+
+    x.domain(data.map(function(d) { return d.id; }));
+    y.domain([0, d3.max(data, function(d) { return d.x1; })]);   
+
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + chart_height+ ")")
+    .call(xAxis)
+    .selectAll("text")
+    .style("text-anchor", "end")
+    .attr("dx", "3")
+    .attr("dy", "5.5");
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("x1");
+
+      svg.selectAll("bar")
+      .data(data)
+      .enter().append("rect")
+      .style("fill", "steelblue")
+      .attr("x", function(d) { return x(d.id); })
+      .attr("width", x.bandwidth())
+      .attr("y", function(d) { return y(d.x1); })
+      .attr("height", function(d) { return chart_height - y(d.x1); });
+  });
