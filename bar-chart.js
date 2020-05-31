196eeb13bbf41986f239eef8325644e27835516c
adfr96 <alexdellorco@gmail.com>
2020-05-31 19:37:49 +0200

    add border

--- bar-chart.js
+++ bar-chart.js
@@ -2,7 +2,7 @@ var margin = {top: 20, right: 20, bottom: 70, left: 40},
     width = 1200 - margin.left - margin.right,
     height = 800 - margin.top - margin.bottom;
 
-var updateTime = 500;
+var updateTime = 750;
 
 var intra_margin = {x:30, y:50};
 var chart_width = width/2 - intra_margin.x;
@@ -12,28 +12,28 @@ var chart_height = height/2 -intra_margin.y;
 // traslazione del chart in base alla variabile
 var var_tx = {
     "x1" : {
-        x_tx: 0,
-        x_ty: chart_height,
-        y_tx: 0,
-        y_ty: 0
+        x_tx: intra_margin.x,
+        x_ty: chart_height+intra_margin.y/2,
+        y_tx: intra_margin.x,
+        y_ty: intra_margin.y/2
     },
     "x2" : {
-        x_tx: chart_width+intra_margin.x,
-        x_ty: chart_height,
-        y_tx: chart_width+intra_margin.x,
-        y_ty: 0
+        x_tx: chart_width+2*intra_margin.x,
+        x_ty: chart_height+intra_margin.y/2,
+        y_tx: chart_width+2*intra_margin.x,
+        y_ty: intra_margin.y/2
     },
     "x3" : {
-        x_tx: 0,
-        x_ty: 2*chart_height+intra_margin.y,
-        y_tx: 0,
-        y_ty: chart_height+intra_margin.y
+        x_tx: intra_margin.x,
+        x_ty: 2*chart_height+intra_margin.y+intra_margin.y/2,
+        y_tx: intra_margin.x,
+        y_ty: chart_height+intra_margin.y+intra_margin.y/2
     },
     "x4" : {
-        x_tx: chart_width+intra_margin.x,
-        x_ty: 2*chart_height+intra_margin.y,
-        y_tx: chart_width+intra_margin.x,
-        y_ty: chart_height+intra_margin.y
+        x_tx: chart_width+2*intra_margin.x,
+        x_ty: 2*chart_height+intra_margin.y+intra_margin.y/2,
+        y_tx: chart_width+2*intra_margin.x,
+        y_ty: chart_height+intra_margin.y+intra_margin.y/2
     }
 };
 var var_color = {
@@ -61,8 +61,18 @@ var yAxis = d3.axisLeft(y).ticks(10);
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
+    .attr("border",1)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "none")
+        .style("stroke", "black")
+        .style("stroke-width", 1)
 
 function updateXScaleDomain(data){
     x.domain(data.map(function(d) { return d.id; }));
@@ -125,14 +135,14 @@ function drawBar(data,variable,chart){
         .attr("y", function(d) { return y(d[variable]); })
         .attr("height", function(d) { return chart_height - y(d[variable]); })
         .attr("transform", "translate("+tx.y_tx+"," + tx.y_ty+ ")");
-
+    /*
     bars.transition().duration(updateTime)
         .attr("x", function(d) { return x(d.id); })
         .attr("width", x.bandwidth())
         .attr("y", function(d) { return y(d[variable]); })
         .attr("height", function(d) { return y(chart_height - d[variable]); })
         .attr("transform", "translate("+tx.y_tx+"," + tx.y_ty+ ")");
-
+    */
 }
 function handleMouseOver(d,i){
     d3.select(this)
@@ -205,7 +215,6 @@ function draw_all(variable){
             transition.selectAll(".bar-"+v)
                 .delay(delay)
                 .attr("x", function(d) {  return x(d.id); })
-                //.attr("width", x.bandwidth())
                 .attr("y", function(d) { return y(d[v]); })
                 .attr("height", function(d) { return chart_height - y(d[v]); })
         } 
